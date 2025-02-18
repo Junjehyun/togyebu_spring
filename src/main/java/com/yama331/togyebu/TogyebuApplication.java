58abcc5574913285e85af5e9bfe916c622d7af14
ジェヒョン <h94051987@gmail.com>
2025-02-18 14:40:30 +0900

    first commit

---
+++ src/main/java/com/yama331/togyebu/TogyebuApplication.java
@@ -0,0 +1,14 @@
+package com.yama331.togyebu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogyebuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogyebuApplication.class, args);
+		//System.out.println("hello world aaaa!!!!");
+	}
+
+}

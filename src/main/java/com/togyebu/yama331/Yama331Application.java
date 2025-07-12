9d84ae2dac5bf04df4c084c9d715bfbd31e165a8
jehyun.jun <h94051987@gmail.com>
2025-07-12 12:09:52 +0900

    first

---
+++ src/main/java/com/togyebu/yama331/Yama331Application.java
@@ -0,0 +1,13 @@
+package com.togyebu.yama331;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yama331Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yama331Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/togyebu/yama331/Yama331ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togyebu.yama331;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yama331ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
